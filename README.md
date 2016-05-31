18a6d8b4ebe54a33f18ab737d5b47f439f28e4fa
erishen <lsun@yinnut.com>
2016-05-31 22:38:53 +0800

    Init

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# PersonalSpace
+# Using React with Browserify
+
+Run `npm install` in the directory to install React from npm. Then run:
+
+```sh
+npm start
+```
+
+to produce `bundle.js` with example code and React.
+
+# http-server -p 8787
---
+++ js/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Index = React.createClass({
+	render(){
+		return (
+			<div>test</div>
+		);
+	}
+});
+
+ReactDOM.render(
+	<Index />,
+	document.body
+);
